553266715fdc52565c9980d3b0090f469bbab082
sflorio <sebastianflorio00@hotmail.com>
2021-11-01 01:34:25 -0300

    Added parser scanner to Api endpoints

--- SmallLanguageApi/src/main/java/com/example/SmallLanguageApi/controllers/ComplierController.java
+++ SmallLanguageApi/src/main/java/com/example/SmallLanguageApi/controllers/ComplierController.java
@@ -1,10 +1,16 @@
 package com.example.SmallLanguageApi.controllers;
 
+import java.io.FileReader;
+import java.io.StringReader;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.SmallLanguageApi.models.*;
+import simplelanguage.Parser;
+import simplelanguage.Scanner;
+
 
 @RestController
 @RequestMapping("/compiler")
@@ -12,16 +18,21 @@ public class ComplierController {
     
 
     @PostMapping("/Compilar")
-    public String Compilar(@RequestBody CompilerRequest request){
-
-        return request.Codigo;
+    public String Compilar(@RequestBody CompilerRequest request) throws Exception{
+    	/*new StringReader(request.Codigo)*/
+    	var result =new Parser(new Scanner(new StringReader(request.Codigo))).parse();
+    	return result.toString();
     }
 
     @PostMapping("/Correr")
-    public String Correr(@RequestBody CompilerRequest request){
-
-
-        return request.Codigo;
+    public String Correr(@RequestBody CompilerRequest request) throws Exception{
+    	try {
+    		var result = new Parser(new Scanner(new StringReader(request.Codigo))).parse();
+    		return result.toString();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return e.getMessage();
+		}
     }
 
 
